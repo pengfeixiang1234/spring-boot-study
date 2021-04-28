8479bbe9d40aead34c6cd37d2fc04fa99867db00
pengfeixiang1234 <13298330375@163.com>
2021-04-28 16:15:09 +0800

    提交项目到github上

---
+++ src/test/java/com/acp/abchina/springbootstudy/SpringBootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acp.abchina.springbootstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
